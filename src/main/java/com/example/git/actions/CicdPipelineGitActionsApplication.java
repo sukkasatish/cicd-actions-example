ec9b4564406bcd58fd81c21d8220b2f2e440700b
sukkasatish <sukkasatish@gmail.com>
2023-04-11 14:15:12 +0530

    Added comments

--- src/main/java/com/example/git/actions/CicdPipelineGitActionsApplication.java
+++ src/main/java/com/example/git/actions/CicdPipelineGitActionsApplication.java
@@ -13,6 +13,14 @@ public class CicdPipelineGitActionsApplication {
     public String getUserDetails(){
         return "Satish Sukka";
     }
+    //commands to move code to created repository
+    /*echo "# cicd-actions-example" >> README.md
+    git init
+    git add README.md
+    git commit -m "gitfirst commit"
+    git branch -M main
+    git remote add origin git@github.com:sukkasatish/cicd-actions-example.git
+    git push -u origin main*/
     public static void main(String[] args) {
         SpringApplication.run(CicdPipelineGitActionsApplication.class, args);
     }
